8687dd6a62961036d408ed63275ee59514f93b7d
422UR4H <tiagosindra@id.uff.br>
2024-02-02 18:26:02 -0300

    :recycle: Refactor: change class exceptions returns

--- src/main/java/com/boardcamp/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/boardcamp/api/exceptions/GlobalExceptionHandler.java
@@ -9,42 +9,42 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler({ BadRequestException.class })
-  public ResponseEntity<Object> handlerBadRequest(BadRequestException exception) {
+  public ResponseEntity<BadRequestException> handlerBadRequest(BadRequestException exception) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception);
   }
 
   @ExceptionHandler({ CustomerNotFoundException.class })
-  public ResponseEntity<Object> handlerCustomerNotFound(CustomerNotFoundException exception) {
+  public ResponseEntity<String> handlerCustomerNotFound(CustomerNotFoundException exception) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
   }
 
   @ExceptionHandler({ GameNotFoundException.class })
-  public ResponseEntity<Object> handlerGameNotFound(GameNotFoundException exception) {
+  public ResponseEntity<String> handlerGameNotFound(GameNotFoundException exception) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
   }
 
   @ExceptionHandler({ RentalNotFoundException.class })
-  public ResponseEntity<Object> handlerRentalNotFound(RentalNotFoundException exception) {
+  public ResponseEntity<String> handlerRentalNotFound(RentalNotFoundException exception) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
   }
 
   @ExceptionHandler({ ConflictGameNameException.class })
-  public ResponseEntity<Object> handlerConflictGameName(ConflictGameNameException exception) {
+  public ResponseEntity<String> handlerConflictGameName(ConflictGameNameException exception) {
     return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
   }
 
   @ExceptionHandler({ ConflictCustomerCpfException.class })
-  public ResponseEntity<Object> handlerConflictCustomerName(ConflictCustomerCpfException exception) {
+  public ResponseEntity<String> handlerConflictCustomerName(ConflictCustomerCpfException exception) {
     return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
   }
 
   @ExceptionHandler({ StockLimitGameRentalException.class })
-  public ResponseEntity<Object> handlerStockLimitGameRental(StockLimitGameRentalException exception) {
+  public ResponseEntity<String> handlerStockLimitGameRental(StockLimitGameRentalException exception) {
     return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(exception.getMessage());
   }
 
   @ExceptionHandler({ RentalFinishedException.class })
-  public ResponseEntity<Object> handlerRentalFinished(RentalFinishedException exception) {
+  public ResponseEntity<String> handlerRentalFinished(RentalFinishedException exception) {
     return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(exception.getMessage());
   }
 
