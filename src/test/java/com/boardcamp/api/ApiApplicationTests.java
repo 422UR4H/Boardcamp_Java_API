d7eb57bb3368a6a411686397cba145570a1256bc
422UR4H <tiagosindra@id.uff.br>
2024-02-12 21:13:46 -0300

    :white_check_mark: Test: add get health

--- src/test/java/com/boardcamp/api/ApiApplicationTests.java
+++ src/test/java/com/boardcamp/api/ApiApplicationTests.java
@@ -1,13 +1,25 @@
 package com.boardcamp.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test") // set application-test.properties
 class ApiApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	void getHealth() {
+		ResponseEntity<String> response = restTemplate.exchange("/health", HttpMethod.GET, null, String.class);
+		assertEquals("I'm Ok!", response.getBody());
 	}
 
 }
