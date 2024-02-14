154f18398133f904ca036e3b9451eea2cecd6494
422UR4H <tiagosindra@id.uff.br>
2024-02-14 01:12:41 -0300

    :bug: Fix: validate rental dto

--- src/main/java/com/boardcamp/api/dtos/RentalDTO.java
+++ src/main/java/com/boardcamp/api/dtos/RentalDTO.java
@@ -1,5 +1,6 @@
 package com.boardcamp.api.dtos;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,12 +11,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RentalDTO {
 
+  @NotNull(message = "Field customerId is mandatory")
   @Positive(message = "Field customerId must be a valid id greater than 0")
   private Long customerId;
 
+  @NotNull(message = "Field gameId is mandatory")
   @Positive(message = "Field gameId must be a valid id greater than 0")
   private Long gameId;
 
+  @NotNull(message = "Field daysRented is mandatory")
   @Positive(message = "Field daysRented must be greater than 0")
   private int daysRented;
 
