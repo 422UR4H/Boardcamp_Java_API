9e3e1e4789c5a00c0d923b7f2ae79329506d7264
422UR4H <tiagosindra@id.uff.br>
2024-02-14 12:59:18 -0300

    :white_check_mark: Test: unit non existing gameId when creating

--- src/test/java/com/boardcamp/api/unit/RentalUnitTests.java
+++ src/test/java/com/boardcamp/api/unit/RentalUnitTests.java
@@ -1,8 +1,11 @@
 package com.boardcamp.api.unit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -11,9 +14,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.boardcamp.api.builders.CustomerBuilder;
+import com.boardcamp.api.builders.GameBuilder;
+import com.boardcamp.api.builders.RentalBuilder;
 import com.boardcamp.api.dtos.RentalDTO;
 import com.boardcamp.api.exceptions.BadRequestException;
+import com.boardcamp.api.exceptions.StockLimitGameRentalException;
+import com.boardcamp.api.models.CustomerModel;
+import com.boardcamp.api.models.GameModel;
 import com.boardcamp.api.repositories.RentalRepository;
+import com.boardcamp.api.services.GameService;
 import com.boardcamp.api.services.RentalService;
 
 @SpringBootTest
@@ -24,6 +34,9 @@ public class RentalUnitTests {
 
   @Mock
   private RentalRepository rentalRepository;
+
+  @Mock
+  private GameService gameService;
 
   @SuppressWarnings("null")
   @Test
@@ -58,6 +71,31 @@ public class RentalUnitTests {
 
     // then
     assertNotNull(exception);
+    verify(rentalRepository, times(0)).save(any());
+  }
+
+  @SuppressWarnings("null")
+  @Test
+  public void givendNonExistingGameId_whenCreating_thenThrowsException() {
+    // given
+    final Long ID = 1L;
+    RentalDTO dto = RentalBuilder.create();
+    CustomerModel customer = new CustomerModel(CustomerBuilder.create());
+    GameModel game = new GameModel(GameBuilder.create());
+
+    customer.setId(ID);
+    game.setId(ID);
+    doReturn(game).when(gameService).findById(anyLong());
+    doReturn(game.getStockTotal()).when(rentalRepository).countByGameIdAndReturnDateIsNull(anyLong());
+
+    // when
+    StockLimitGameRentalException exception = assertThrows(
+      StockLimitGameRentalException.class,
+        () -> rentalService.create(dto));
+
+    // then
+    assertNotNull(exception);
+    assertEquals("Stock limit for renting this game", exception.getMessage());
     verify(rentalRepository, times(0)).save(any());
   }
 
