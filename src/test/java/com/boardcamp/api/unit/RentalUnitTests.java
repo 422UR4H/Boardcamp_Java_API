d2dcefa1d0692881eba542aa4538f9dbb9a45310
422UR4H <tiagosindra@id.uff.br>
2024-02-14 13:14:24 -0300

    :white_check_mark: Test: unit valid rental when creating

--- src/test/java/com/boardcamp/api/unit/RentalUnitTests.java
+++ src/test/java/com/boardcamp/api/unit/RentalUnitTests.java
@@ -22,7 +22,9 @@ import com.boardcamp.api.exceptions.BadRequestException;
 import com.boardcamp.api.exceptions.StockLimitGameRentalException;
 import com.boardcamp.api.models.CustomerModel;
 import com.boardcamp.api.models.GameModel;
+import com.boardcamp.api.models.RentalModel;
 import com.boardcamp.api.repositories.RentalRepository;
+import com.boardcamp.api.services.CustomerService;
 import com.boardcamp.api.services.GameService;
 import com.boardcamp.api.services.RentalService;
 
@@ -37,6 +39,9 @@ public class RentalUnitTests {
 
   @Mock
   private GameService gameService;
+
+  @Mock
+  private CustomerService customerService;
 
   @SuppressWarnings("null")
   @Test
@@ -90,13 +95,39 @@ public class RentalUnitTests {
 
     // when
     StockLimitGameRentalException exception = assertThrows(
-      StockLimitGameRentalException.class,
+        StockLimitGameRentalException.class,
         () -> rentalService.create(dto));
 
     // then
     assertNotNull(exception);
     assertEquals("Stock limit for renting this game", exception.getMessage());
     verify(rentalRepository, times(0)).save(any());
+  }
+
+  @SuppressWarnings("null")
+  @Test
+  public void givenValidRental_whenCreating_thenCreatesRental() {
+    // given
+    final Long ID = 1L;
+    RentalDTO dto = RentalBuilder.create();
+    CustomerModel customer = new CustomerModel(CustomerBuilder.create());
+    GameModel game = new GameModel(GameBuilder.create());
+    RentalModel rental = new RentalModel(dto.getDaysRented(), game.getPricePerDay(), customer, game);
+
+    customer.setId(ID);
+    game.setId(ID);
+    doReturn(game).when(gameService).findById(anyLong());
+    doReturn(0).when(rentalRepository).countByGameIdAndReturnDateIsNull(anyLong());
+    doReturn(customer).when(customerService).findById(anyLong());
+    doReturn(rental).when(rentalRepository).save(rental);
+
+    // when
+    RentalModel result = rentalService.create(dto);
+
+    // then
+    assertNotNull(result);
+    assertEquals(rental, result);
+    verify(rentalRepository, times(1)).save(any());
   }
 
 }
