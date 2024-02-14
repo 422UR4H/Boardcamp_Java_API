817b6ca973e146f10b7a6b5d79e5682d23c75ab0
422UR4H <tiagosindra@id.uff.br>
2024-02-14 13:03:48 -0300

    :truck: Rename: fix unit rental test name

--- src/test/java/com/boardcamp/api/unit/RentalUnitTests.java
+++ src/test/java/com/boardcamp/api/unit/RentalUnitTests.java
@@ -76,7 +76,7 @@ public class RentalUnitTests {
 
   @SuppressWarnings("null")
   @Test
-  public void givendStockLimitGameRental_whenCreating_thenThrowsException() {
+  public void givenStockLimitGameRental_whenCreating_thenThrowsException() {
     // given
     final Long ID = 1L;
     RentalDTO dto = RentalBuilder.create();
